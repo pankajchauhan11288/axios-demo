4839ee5f97f10e302a008c9225ee8fc6cc8e59eb
pankajchauhan11288 <60954593+pankajchauhan11288@users.noreply.github.com>
2022-05-14 20:01:27 +0530

    all changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import ToDoDetails from "./Components/ToDoDetails";
+import TodoList from "./Components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/todos/:id" element={<ToDoDetails />} />
+        </Routes>
+        <Routes>
+          <Route exact path="/" element={<TodoList />} />
+        </Routes>
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
